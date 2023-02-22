86e4f24e24aff0f5b3873acb2e0bc878c9b1233f
ashokzarmariyaa <ashok.zarmariya@permissionles.org>
2023-02-22 19:16:38 +0530

    implement registerd user controller

--- src/main/java/com/zos/controller/UserController.java
+++ src/main/java/com/zos/controller/UserController.java
@@ -1,5 +1,29 @@
 package com.zos.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zos.exception.UserException;
+import com.zos.modal.User;
+import com.zos.service.UserService;
+
+@RestController
 public class UserController {
+	@Autowired
+	private UserService userService;
+	
+	@PostMapping("/signup")
+	public ResponseEntity<User> registerUserHandler(@RequestBody User user) throws UserException{
+		
+		User registeredUser=userService.registerUser(user);
+		return new ResponseEntity<User>(registeredUser,HttpStatus.OK);
+	}
+	
+	
+	
 
 }
