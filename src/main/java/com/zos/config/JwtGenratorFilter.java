6827dd889bea9e01443dec6f2d93919b03d8fc72
ashokzarmariyaa <ashok.zarmariya@permissionles.org>
2023-02-22 16:30:25 +0530

    add json web token filter

--- src/main/java/com/zos/config/JwtGenratorFilter.java
+++ src/main/java/com/zos/config/JwtGenratorFilter.java
@@ -1,13 +1,16 @@
 package com.zos.config;
 
 import java.io.IOException;
+import java.util.Collection;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.crypto.SecretKey;
 
 
 import org.springframework.security.core.Authentication;
-
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 import org.springframework.web.filter.OncePerRequestFilter;
@@ -34,7 +37,7 @@ public class JwtGenratorFilter extends OncePerRequestFilter{
 				.setIssuer("zos academy")
 				.setSubject("jwt_token")
 				.claim("username",authentication.getName())
-				.claim("authorities",authentication.getAuthorities())
+				.claim("authorities", populateAuthorities(authentication.getAuthorities()))
 				.setIssuedAt(new Date())
 				.setExpiration(new Date(new Date().getTime()+ 48900000))
 				.signWith(key).compact();
@@ -46,6 +49,17 @@ public class JwtGenratorFilter extends OncePerRequestFilter{
 		filterChain.doFilter(request, response);
 	}
 	
+    private String populateAuthorities(Collection<? extends GrantedAuthority> collection) {
+        
+    	Set<String> authoritiesSet = new HashSet<>();
+        
+        for (GrantedAuthority authority : collection) {
+            authoritiesSet.add(authority.getAuthority());
+        }
+        return String.join(",", authoritiesSet);
+   
+    
+    }
 	
 	@Override
 	protected boolean shouldNotFilter(HttpServletRequest request) throws ServletException {
--- src/main/java/com/zos/config/JwtValidatorFilter.java
+++ src/main/java/com/zos/config/JwtValidatorFilter.java
@@ -1,15 +1,26 @@
 package com.zos.config;
-
+import java.util.List;
 import java.io.IOException;
 
+import javax.crypto.SecretKey;
+
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.security.core.GrantedAuthority;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
+
 
 public class JwtValidatorFilter extends OncePerRequestFilter {
 
@@ -18,12 +29,40 @@ public class JwtValidatorFilter extends OncePerRequestFilter {
 			throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		
-		Authentication authentication=SecurityContextHolder.getContext().getAuthentication();
+		String jwt =response.getHeader(SecurityConstant.HEADER);
+	
+		if(jwt != null) {
+			
+			try {
+
+				jwt=jwt.substring(7);
+				
+				SecretKey key=Keys.hmacShaKeyFor(SecurityConstant.JWT_KEY.getBytes());
+				
+				Claims claim=Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(jwt).getBody();
+				
+				String username=String.valueOf(claim.get("username"));
+				String authorities=String.valueOf(claim.get("authorities"));
+				
+				List<GrantedAuthority> auths=AuthorityUtils.commaSeparatedStringToAuthorityList(authorities);
+				
+				Authentication auth=new UsernamePasswordAuthenticationToken(username, null,auths);
+				
+			} catch (Exception e) {
+				throw new BadCredentialsException("invalid token");
+				// TODO: handle exception
+			}
+			
+			
+			
+		}
+		filterChain.doFilter(request, response);
 		
-		String jwt =(String) authentication.getCredentials();
 		
-		
-		
+	}
+	
+	protected boolean shouldNotFilter(HttpServletRequest request) {
+		return request.getServletPath().equals("/sigin");
 	}
 
 }
