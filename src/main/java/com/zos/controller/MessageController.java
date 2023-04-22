2cd474f47ca42d7f9b9a5354513f70ce3d2aa23e
Ashok Zarmariya <ashokzarmariya47@gmail.com>
2023-04-22 12:27:10 +0530

    write message service and create send message api

---
+++ src/main/java/com/zos/controller/MessageController.java
@@ -0,0 +1,43 @@
+package com.zos.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.zos.controller.mapper.MessageDtoMapper;
+import com.zos.dto.MessageDto;
+import com.zos.exception.ChatException;
+import com.zos.exception.UserException;
+import com.zos.modal.Message;
+import com.zos.modal.User;
+import com.zos.request.SendMessageRequest;
+import com.zos.service.MessageService;
+import com.zos.service.UserService;
+
+@RestController
+@RequestMapping("/api/messages")
+public class MessageController {
+
+	@Autowired
+	private MessageService messageService;
+	
+	@Autowired
+	private UserService userService;
+	
+	public ResponseEntity<MessageDto> sendMessageHandler(@RequestHeader("Authorization")String jwt,  @RequestBody SendMessageRequest req) throws UserException, ChatException{
+		
+		User reqUser=userService.findUserProfile(jwt);
+		
+		req.setUserId(reqUser.getId());
+		
+		Message message=messageService.sendMessage(req);
+		
+		MessageDto messageDto=MessageDtoMapper.toMessageDto(message);
+		
+		return new ResponseEntity<MessageDto>(messageDto,HttpStatus.OK);
+	}
+}
